96191cb48a8eb37c68930bcf9e93429fd5d979f1
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-13 08:43:27 -0300

    design body background and include header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,11 @@ import './styles.css'
 
 export function Home() {
     return (
-        <></>
+        <>
+            <header>
+                <h1>Conheça nossos planos</h1>
+                <h2>Conheça nossas soluções e escolha o plano ideal para o seu próximo projeto</h2>
+            </header>
+        </>
     )
 }
